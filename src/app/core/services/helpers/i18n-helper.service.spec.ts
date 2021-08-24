97a514f24552349d4a74cd40236e617ef12f3ee1
Joseph <joseph.morant@soprasteria.com>
2021-08-24 19:39:26 +0200

    fix(i18n-helper): add required imports to spec file

--- src/app/core/services/helpers/i18n-helper.service.spec.ts
+++ src/app/core/services/helpers/i18n-helper.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { I18nHelper } from './i18n-helper.service';
 
 describe('I18nHelperService', () => {
   let service: I18nHelper;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
+    });
     service = TestBed.inject(I18nHelper);
   });
 
