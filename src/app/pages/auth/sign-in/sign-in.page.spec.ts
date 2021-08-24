c6192b0b2708256331b25cd65a19f667d18de575
Joseph <joseph.morant@soprasteria.com>
2021-08-24 19:40:01 +0200

    fix(sign-in): add required imports and providers to spec file

--- src/app/pages/auth/sign-in/sign-in.page.spec.ts
+++ src/app/pages/auth/sign-in/sign-in.page.spec.ts
@@ -1,22 +1,26 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { SignInPage } from './sign-in.page';
 
 describe('SignInPage', () => {
   let component: SignInPage;
   let fixture: ComponentFixture<SignInPage>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SignInPage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [SignInPage],
+        imports: [IonicModule.forRoot(), TranslateModule.forRoot()],
+        providers: [{ provide: FormBuilder }],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(SignInPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(SignInPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
