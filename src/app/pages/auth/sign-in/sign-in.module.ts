dd384a9494fde4388d86cfaa60dfe8697d68383a
Joseph <joseph.morant@soprasteria.com>
2021-08-24 19:37:38 +0200

    feat(sign-in): show form validaton errors

--- src/app/pages/auth/sign-in/sign-in.module.ts
+++ src/app/pages/auth/sign-in/sign-in.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { SignInPageRoutingModule } from './sign-in-routing.module';
 import { SignInPage } from './sign-in.page';
 import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     SignInPageRoutingModule,
     TranslateModule.forChild(),
+    SharedModule,
   ],
   declarations: [SignInPage],
 })
--- src/app/pages/auth/sign-in/sign-in.page.ts
+++ src/app/pages/auth/sign-in/sign-in.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { emailValidator } from 'src/app/shared/validators/email-validator.directive';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.page.scss'],
 })
 export class SignInPage implements OnInit {
+  signInForm: FormGroup;
+  isRemindEnabled = false;
+  isSubmitButtonClicked = false;
 
-  constructor() { }
+  constructor(private readonly fb: FormBuilder) {}
 
   ngOnInit() {
+    this.signInForm = this.fb.group({
+      email: [null, [Validators.required, emailValidator()]],
+      password: [null, [Validators.required, Validators.minLength(5)]],
+    });
   }
 
+  async onSubmit() {
+    if (this.signInForm.invalid) {
+      this.isSubmitButtonClicked = true;
+      return;
+    }
+
+    console.log('🚀 ~ SignInPage ~ onSubmit ~ OK');
+    const logInDto = this.signInForm.value;
+    // TODO: API call
+  }
 }
