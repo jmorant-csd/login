370148db73bd08caeceaabb00e15d0cd5d14275c
Joseph <joseph.morant@soprasteria.com>
2021-08-24 19:34:01 +0200

    style(auth): sign-in page

--- src/app/pages/auth/sign-in/sign-in.module.ts
+++ src/app/pages/auth/sign-in/sign-in.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { SignInPageRoutingModule } from './sign-in-routing.module';
-
 import { SignInPage } from './sign-in.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SignInPageRoutingModule
+    SignInPageRoutingModule,
+    TranslateModule.forChild(),
   ],
-  declarations: [SignInPage]
+  declarations: [SignInPage],
 })
 export class SignInPageModule {}
